8a802a5696f809a765d03df86eceb1b20deb9431
SerenityScript <yuliasiebrandt89@gmail.com>
2025-12-07 18:16:37 +0100

    Feature AddDish

---
+++ src/features/AddDish/ui/AddDish.jsx
@@ -0,0 +1,21 @@
+import { DishForm } from "@/entities/Dish/ui/DishForm/DishForm";
+
+export const AddDish = ({ dishes = [], onChangeDishes, responsibleOptions }) => {
+  const handleSubmit = (formValues) => {
+    const newDish = {
+      id: Date.now().toString() + Math.random().toString(16),
+      ...formValues,
+    };
+
+    onChangeDishes?.([...dishes, newDish]);
+  };
+
+  return (
+    <DishForm
+      initialValues={{ name: "", responsible: "me", note: "" }}
+      responsibleOptions={responsibleOptions}
+      submitLabel="Gericht hinzufügen"
+      onSubmit={handleSubmit}
+    />
+  );
+};
