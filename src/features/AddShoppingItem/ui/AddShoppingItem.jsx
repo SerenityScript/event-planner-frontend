1071867f0fc4bb8a17145b7e4ccf0e24670e4c2c
SerenityScript <yuliasiebrandt89@gmail.com>
2025-12-08 12:43:15 +0100

    feature AddShoppingItem created

---
+++ src/features/AddShoppingItem/ui/AddShoppingItem.jsx
@@ -0,0 +1,26 @@
+import { ShoppingForm } from "@/entities/Shopping/ui/ShoppingForm/ShoppingForm";
+
+export const AddShoppingItem = ({
+  items = [],
+  onChangeItems,
+  categoryOptions,
+}) => {
+  const handleSubmit = (formValues) => {
+    const newItem = {
+      id: Date.now().toString() + Math.random().toString(16),
+      ...formValues,
+      bought: false,
+    };
+
+    onChangeItems?.([...items, newItem]);
+  };
+
+  return (
+    <ShoppingForm
+      initialValues={{ name: "", qty: "", category: "food" }}
+      categoryOptions={categoryOptions}
+      submitLabel="Hinzufügen"
+      onSubmit={handleSubmit}
+    />
+  );
+};
