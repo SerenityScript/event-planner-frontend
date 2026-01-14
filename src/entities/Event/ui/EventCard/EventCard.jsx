c060a908ff114b27634019f733a551473186ad95
SerenityScript <yuliasiebrandt89@gmail.com>
2026-01-14 09:01:28 +0100

    connect to backend

--- src/entities/Event/ui/EventCard/EventCard.jsx
+++ src/entities/Event/ui/EventCard/EventCard.jsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
+import { formatDateDE } from "@/shared/utils";
 
-export const EventCard = ({ event }) => {
+export const EventCard = ({ event, onDelete }) => {
   if (!event) return null;
 
   return (
     <div style={{ border: "1px solid #ccc", padding: "12px", marginBottom: "8px" }}>
-      <h2>{event.title}</h2>
-      <p>Datum: {event.date}</p>
+      <h2>{event.name}</h2>
 
-      <Link to={`/events/${event.id}`}>Öffnen</Link>
-      <button>Delete Event</button>
+      <p>
+        Datum: {formatDateDE(event.date)} {event.time && `• ${event.time}`}
+      </p>
+
+      <div style={{ display: "flex", gap: "8px" }}>
+        <Link to={`/events/${event._id}`}>Öffnen</Link>
+
+        <button onClick={() => onDelete?.(event._id)}>
+          Delete Event
+        </button>
+      </div>
     </div>
   );
 };
