b0c7cf535ee6a42373723217c6b81f96b4f93eeb
SerenityScript <yuliasiebrandt89@gmail.com>
2026-01-21 15:57:24 +0100

    eventcard updated

--- src/entities/Event/ui/EventCard/EventCard.jsx
+++ src/entities/Event/ui/EventCard/EventCard.jsx
@@ -1,24 +1,36 @@
-import { Link } from "react-router-dom";
 import { formatDateDE } from "@/shared/utils";
+import { useNavigate } from "react-router-dom";
 
 export const EventCard = ({ event, onDelete }) => {
+  const navigate = useNavigate();
   if (!event) return null;
 
   return (
-    <div style={{ border: "1px solid #ccc", padding: "12px", marginBottom: "8px" }}>
+    <div
+      onClick={() => navigate(`/events/${event._id}`)}
+      style={{
+        border: "1px solid #ccc",
+        padding: "12px",
+        marginBottom: "8px",
+        cursor: "pointer",
+      }}
+    >
       <h2>{event.name}</h2>
 
       <p>
         Datum: {formatDateDE(event.date)} {event.time && `• ${event.time}`}
       </p>
 
-      <div style={{ display: "flex", gap: "8px" }}>
-        <Link to={`/events/${event._id}`}>Öffnen</Link>
-
-        <button onClick={() => onDelete?.(event._id)}>
-          Delete Event
-        </button>
-      </div>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          if (confirm("Delete this event?")) {
+            onDelete?.(event._id);
+          }
+        }}
+      >
+        Delete Event
+      </button>
     </div>
   );
 };
